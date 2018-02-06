7532bf3d159a3237641053a29fcfe2ac09db9fc4
Abe Fetterman <abe@nomiku.com>
2018-02-05 17:15:02 -0800

    game basics, start with rendering

---
+++ rendering.py
@@ -0,0 +1,86 @@
+# based on https://github.com/openai/gym/blob/master/gym/envs/classic_control/rendering.py
+
+import pyglet
+from pyglet.gl import *
+
+class Viewer(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.window = pyglet.window.Window(width=width, height=height)
+        self.window.on_close = self.window_closed_by_user
+        self.isopen = True
+        self.geoms = []
+        self.onetime_geoms = []
+        self.transform = Transform()
+
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    def close(self):
+        self.window.close()
+
+    def window_closed_by_user(self):
+        self.isopen = False
+
+    def set_bounds(self, left, right, bottom, top):
+        assert right > left and top > bottom
+        scalex = self.width/(right-left)
+        scaley = self.height/(top-bottom)
+        self.transform = Transform(
+            translation=(-left*scalex, -bottom*scaley),
+            scale=(scalex, scaley))
+
+    def add_geom(self, geom):
+        self.geoms.append(geom)
+
+    def add_onetime(self, geom):
+        self.onetime_geoms.append(geom)
+
+    def render(self, return_rgb_array=False):
+        glClearColor(1,1,1,1)
+        self.window.clear()
+        self.window.switch_to()
+        self.window.dispatch_events()
+        self.transform.enable()
+        for geom in self.geoms:
+            geom.render()
+        for geom in self.onetime_geoms:
+            geom.render()
+        self.transform.disable()
+        arr = None
+        if return_rgb_array:
+            buffer = pyglet.image.get_buffer_manager().get_color_buffer()
+            image_data = buffer.get_image_data()
+            arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
+            # In https://github.com/openai/gym-http-api/issues/2, we
+            # discovered that someone using Xmonad on Arch was having
+            # a window of size 598 x 398, though a 600 x 400 window
+            # was requested. (Guess Xmonad was preserving a pixel for
+            # the boundary.) So we use the buffer height/width rather
+            # than the requested one.
+            arr = arr.reshape(buffer.height, buffer.width, 4)
+            arr = arr[::-1,:,0:3]
+        self.window.flip()
+        self.onetime_geoms = []
+        return arr if return_rgb_array else self.isopen
+
+
+class Transform:
+    def __init__(self, translation=(0.0, 0.0), rotation=0.0, scale=(1,1)):
+        self.set_translation(*translation)
+        self.set_rotation(rotation)
+        self.set_scale(*scale)
+    def enable(self):
+        glPushMatrix()
+        glTranslatef(self.translation[0], self.translation[1], 0) # translate to GL loc ppint
+        glRotatef(RAD2DEG * self.rotation, 0, 0, 1.0)
+        glScalef(self.scale[0], self.scale[1], 1)
+    def disable(self):
+        glPopMatrix()
+    def set_translation(self, newx, newy):
+        self.translation = (float(newx), float(newy))
+    def set_rotation(self, new):
+        self.rotation = float(new)
+    def set_scale(self, newx, newy):
+        self.scale = (float(newx), float(newy))
---
+++ snake.py
@@ -0,0 +1,134 @@
+import math
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+import pyglet
+
+STATE_W = 30
+STATE_H = 30
+VIDEO_W = 300
+VIDEO_H = 300
+WINDOW_W = 600
+WINDOW_H = 600
+
+SCALE = WINDOW_W/STATE_W
+
+INIT_SNAKE_LENGTH = 3
+
+DIRECTIONS_DICT = {
+    0: (0,0),
+    1: (0,1),
+    2: (0,-1),
+    3: (-1,0),
+    4: (1,0)
+}
+
+class SnakeEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array', 'state_pixels'],
+        'video.frames_per_second': 30
+    }
+
+    def __init__(self):
+        self.viewer = None
+        self.snake = None
+
+        self.action_space = spaces.Discrete(5) #none, u, d, l, r
+        self.observation_space = spaces.Box(low=0, high=255, shape=(STATE_H, STATE_W, 3), dtype=np.uint8)
+
+        self.seed()
+        self.reset()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        length = INIT_SNAKE_LENGTH
+        init_x = self.np_random.randint(STATE_W - 2*length) + length
+        init_y = self.np_random.randint(STATE_H - 2*length) + length
+        self.snake_dir = 1+self.np_random.randint(4)
+        dx, dy = DIRECTIONS_DICT[self.snake_dir]
+
+        self.snake = [(init_x, init_y)]
+        self.snake_length = length
+
+        self._place_food()
+
+        #return self._render()[0]
+
+    def _place_food(self):
+        self.food = self.snake[0]
+        while self.food in self.snake:
+            food_x = self.np_random.randint(STATE_W)
+            food_y = self.np_random.randint(STATE_H)
+            self.food = (food_x, food_y)
+
+    def step(self, action):
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+
+        if (action > 0):
+            self.snake_dir = action
+
+        cx,cy = self.snake[0]
+        dx, dy = DIRECTIONS_DICT[self.snake_dir]
+
+        head = (cx + dx, cy + dy)
+
+        done = False
+        reward = 0.0
+
+        # check for wall hit
+        if (head[0] < 0 or head[0] >= STATE_W):
+            done = True
+        if (head[1] < 0 or head[1] >= STATE_H):
+            done = True
+
+        # check for self hit
+        if head in self.snake:
+            done = True
+
+        if (not done):
+            self.snake.insert(0, head)
+
+            # eat food
+            if (head == self.food):
+                reward = 1.0
+                self.snake_length += 2
+                self._place_food()
+
+            # maintain length
+            while (len(self.snake) > self.snake_length):
+                self.snake.pop()
+
+
+        self.state = self._render()[0]
+        return np.array(self.state), reward, done, {}
+
+
+    def render(self, mode='human'):
+
+        if self.viewer is None:
+            import rendering
+            self.viewer = rendering.Viewer(WINDOW_W, WINDOW_H)
+            label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=WINDOW_W//2, y=WINDOW_H//2,
+                          anchor_x='center', anchor_y='center')
+            label.draw()
+            self.transform = rendering.Transform()
+
+
+    def close(self):
+        if self.viewer: self.viewer.close()
+
+
+if __name__=="__main__":
+    env = SnakeEnv()
+    try:
+        while True:
+            env.render()
+    except (KeyboardInterrupt):
+        env.close()
